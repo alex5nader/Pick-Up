82cf5f2bb7a8b854075223d97a7000e1654ad4fc
Alex Habich <me@alexnader.dev>
2021-01-06 20:34:04 -0600

    Refactor command into separate file

--- src/main/java/dev/alexnader/pick_up/common/PickUp.java
+++ src/main/java/dev/alexnader/pick_up/common/PickUp.java
@@ -1,47 +1,24 @@
 package dev.alexnader.pick_up.common;
 
-import dev.alexnader.pick_up.common.item.HeldItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Hand;
-
-import static net.minecraft.server.command.CommandManager.literal;
 
 public class PickUp implements ModInitializer {
     public static PickUpMeta META;
     public static PickUpItems ITEMS;
+    public static PickUpCommands COMMANDS;
 
     @Override
     public void onInitialize() {
         META = new PickUpMeta();
         ITEMS = new PickUpItems();
+        COMMANDS = new PickUpCommands();
 
         //noinspection CodeBlock2Expr // more readable with explicit block
         CommandRegistrationCallback.EVENT.register((dispatcher, isDedi) -> {
-            dispatcher.register(literal(META.id("discard").toString()).executes(ctx -> {
-                ServerCommandSource source = ctx.getSource();
-                PlayerEntity sender = source.getPlayer();
-                ItemStack stack = sender.getStackInHand(Hand.MAIN_HAND);
-
-                if (!ITEMS.isHeldItem(stack.getItem())) {
-                    source.sendError(new TranslatableText("pick_up.command.discard.not_holding"));
-                    return -1;
-                }
-
-                source.sendFeedback(
-                    new TranslatableText("pick_up.command.discard.success")
-                        .append(new TranslatableText(((HeldItem) stack.getItem()).getHeldTranslationKey(stack))),
-                    false
-                );
-                stack.setCount(0);
-                return 1;
-            }));
+            dispatcher.register(COMMANDS.DISCARD);
         });
 
         UseEntityCallback.EVENT.register(PickUpPickingUp::tryPickUpEntity);
---
+++ src/main/java/dev/alexnader/pick_up/common/PickUpCommands.java
@@ -0,0 +1,38 @@
+package dev.alexnader.pick_up.common;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import dev.alexnader.pick_up.common.item.HeldItem;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Hand;
+
+import static dev.alexnader.pick_up.common.PickUp.ITEMS;
+import static dev.alexnader.pick_up.common.PickUp.META;
+import static net.minecraft.server.command.CommandManager.literal;
+
+public class PickUpCommands {
+    public LiteralArgumentBuilder<ServerCommandSource> DISCARD = literal(META.id("discard").toString()).executes(PickUpCommands::discard);
+
+    private static int discard(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        ServerCommandSource source = context.getSource();
+        PlayerEntity sender = source.getPlayer();
+        ItemStack stack = sender.getStackInHand(Hand.MAIN_HAND);
+
+        if (!ITEMS.isHeldItem(stack.getItem())) {
+            source.sendError(new TranslatableText("pick_up.command.discard.not_holding"));
+            return -1;
+        }
+
+        source.sendFeedback(
+            new TranslatableText("pick_up.command.discard.success")
+                .append(new TranslatableText(((HeldItem) stack.getItem()).getHeldTranslationKey(stack))),
+            false
+        );
+        stack.setCount(0);
+        return 1;
+    }
+}
